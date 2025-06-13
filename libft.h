8c1627716de5b63bd6a450d2d4a7c9340c9e60f4
Zakaria Farah <zakaria.farah55@outlook.com>
2025-06-13 14:25:39 +0300

    norm fixes

--- libft.h
+++ libft.h
@@ -1,8 +1,8 @@
 #ifndef LIBFT_H
-#define LIBFT_H
+# define LIBFT_H
 
-#include "standard/standard.h"
-#include "printf/ft_printf.h"
-#include "linked_list/linked_list.h"
-#include "get_next_line/get_next_line.h"
+# include "get_next_line/get_next_line.h"
+# include "linked_list/linked_list.h"
+# include "printf/ft_printf.h"
+# include "standard/standard.h"
 #endif
