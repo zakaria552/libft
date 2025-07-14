d9c7ceaeb6978e9370fbeddf25ac0570aa86e85a
Zakaria Farah <zakaria.fara552@gmail.com>
2025-07-14 13:19:35 +0300

    added missing header for get next line

--- libft.h
+++ libft.h
@@ -1,9 +1,22 @@
-#ifndef LIBFT_H
-#define LIBFT_H
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zfarah <zfarah@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/14 13:14:58 by zfarah            #+#    #+#             */
+/*   Updated: 2025/07/14 13:17:54 by zfarah           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-#include "standard/standard.h"
-#include "printf/ft_printf.h"
-#include "linked_list/linked_list.h"
-#include "queue/queue.h"
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include "get_next_line/get_next_line.h"
+# include "linked_list/linked_list.h"
+# include "printf/ft_printf.h"
+# include "queue/queue.h"
+# include "standard/standard.h"
 
 #endif
