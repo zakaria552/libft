b10adee98a27e5a9aa84bbc0ed9c3b400069788c
Zakaria Farah <113371958+zakaria552@users.noreply.github.com>
2026-01-22 13:11:38 +0200

    Fix return statement in ft_memchr function

--- standard/ft_memchr.c
+++ standard/ft_memchr.c
@@ -21,7 +21,7 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	{
 		if (((unsigned char *)s)[i] == (unsigned char)c)
 		{
-			return (void *)&(s + i);
+			return (void *)(s + i);
 		}
 		i++;
 	}
